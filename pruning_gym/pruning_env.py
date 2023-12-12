Fixed rotation error

pruning_gym/pruning_env.py
@@ -362,9 +362,9 @@ class PruningEnv(gym.Env):
             #pointing -> z
             #perpendicular -> x
         current_or_mat = np.array(self.pyb.con.getMatrixFromQuaternion(self.observation_info['achieved_or_quat'])).reshape(3, 3)
-        theta, rf = Reward.get_angular_distance_to_goal(current_or_mat, self.tree_goal_branch, self.observation_info['achieved_pos'], self.observation_info['desired_pos'])
+        theta, rf = Reward.get_angular_distance_to_goal(current_or_mat.T, self.tree_goal_branch, self.observation_info['achieved_pos'], self.observation_info['desired_pos'])
         print(theta)
-        self.pyb.visualize_rot_mat(current_or_mat, self.observation_info['achieved_pos'])
+        self.pyb.visualize_rot_mat(current_or_mat.T, self.observation_info['achieved_pos'])
         self.pyb.visualize_rot_mat(rf, self.observation_info['desired_pos'])
         # infos['episode'] = {"l": self.stepCounter,  "r": reward}
         infos['velocity'] = np.linalg.norm(self.action)

pruning_gym/reward_utils.py
@@ -155,8 +155,9 @@ class Reward:
         #get pointing vector
         #red branch
         #blue poitinh
+        #TODO: Decide on system for rot mat
         pointing_vector = Reward.compute_perpendicular_projection(achieved_pos, desired_pos, branch_vector + desired_pos)
-        pointing_vector = -pointing_vector / np.linalg.norm(pointing_vector)
+        pointing_vector = pointing_vector / np.linalg.norm(pointing_vector)
         #get perpendicular vector
         perpendicular_vector = -branch_vector
         perpendicular_vector = perpendicular_vector / np.linalg.norm(perpendicular_vector)
@@ -167,6 +168,6 @@ class Reward:
         #compute difference in rotation matrix
         diff = np.matmul(current_or_mat, rf.T)
         #get theta
-        theta = ((np.trace(diff) - 1) / 2)
+        theta = np.arccos((np.trace(diff) - 1) / 2)
 
         return theta, rf