9daff6e2104bd62911e98702d9f823b03c247593
Abhinav Jain <abhinav98jain@gmail.com>
2023-12-13 17:52:43 -0800

    Mult action weights by 10

--- algo/PPOLSTMAE/policies.py
+++ algo/PPOLSTMAE/policies.py
@@ -221,6 +221,7 @@ class ActorCriticPolicySquashed(BasePolicy):
         self.action_net, self.log_std = self.action_dist.proba_distribution_net(
             latent_dim=latent_dim_pi, log_std_init=self.log_std_init
         )
+        self.action_net.weight.data *= 10
         # if isinstance(self.action_dist, DiagGaussianDistribution):
         #     self.action_net, self.log_std = self.action_dist.proba_distribution_net(
         #         latent_dim=latent_dim_pi, log_std_init=self.log_std_init
