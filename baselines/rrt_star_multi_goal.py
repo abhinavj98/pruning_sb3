853180158d4e07ae5cab3e690a51260edea68636
Abhinav Jain <abhinav98jain@gmail.com>
2024-07-10 14:34:06 -0700

    Implement informed RRT star

---
+++ baselines/rrt_star_multi_goal.py
@@ -0,0 +1,90 @@
+from random import random
+from time import time
+from pybullet_planning.motion_planners.rrt_star import OptimalNode, safe_path
+from pybullet_planning.motion_planners.utils import INF, argmin, elapsed_time
+
+EPSILON = 1e-6
+PRINT_FREQUENCY = 100
+
+def rrt_star_multi_goal(start, goal_fn, distance_fn, sample_fn, extend_fn, collision_fn, radius, is_goal_fn,
+             max_time=INF, max_iterations=INF, goal_probability=.2, informed=True, verbose=False, draw_fn=None):
+    """
+    Modifications to RRT* to handle a variety of goals. Replace goal configuration with a goal sampling function.
+    :param start: Start configuration - conf
+    :param goal: Function to sample end configuration - goal_fn()->conf
+    :param distance_fn: Distance function - distance_fn(q1, q2)->float
+    :param sample_fn: Sample function - sample_fn()->conf
+    :param extend_fn: Extension function - extend_fn(q1, q2)->[q', ..., q"]
+    :param collision_fn: Collision function - collision_fn(q)->bool
+    :param max_time: Maximum runtime - float
+    :return: Path [q', ..., q"] or None if unable to find a solution
+    """
+    if collision_fn(start):
+        return None
+    nodes = [OptimalNode(start)]
+    goal_n = None
+    start_time = time()
+    iteration = 0
+    while (elapsed_time(start_time) < max_time) and (iteration < max_iterations):
+        do_goal = goal_n is None and (iteration == 0 or random() < goal_probability)
+        s = goal_fn(config = True) if do_goal else sample_fn()
+        if do_goal:
+            goal = s
+            if collision_fn(goal):
+                continue
+
+
+        # Informed RRT*
+        if informed and (goal_n is not None) and (distance_fn(start, s) + distance_fn(s, goal) >= goal_n.cost):
+            continue
+        if iteration % PRINT_FREQUENCY == 0:
+            success = goal_n is not None
+            cost = goal_n.cost if success else INF
+            if verbose:
+                print('Iteration: {} | Time: {:.3f} | Success: {} | {} | Cost: {:.3f}'.format(
+                    iteration, elapsed_time(start_time), success, do_goal, cost))
+        iteration += 1
+
+        nearest = argmin(lambda n: distance_fn(n.config, s), nodes)
+        path = safe_path(extend_fn(nearest.config, s), collision_fn)
+        if len(path) == 0:
+            continue
+        new = OptimalNode(path[-1], parent=nearest, d=distance_fn(
+            nearest.config, path[-1]), path=path[:-1], iteration=iteration)
+        # if safe and do_goal:
+        #TODO: Replace to check if any node is within epsilon of goal
+        if is_goal_fn(new.config):
+            goal_n = new
+            goal_n.set_solution(True)
+        # if do_goal and (distance_fn(new.config, goal) < EPSILON):
+        #     goal_n = new
+        #     goal_n.set_solution(True)
+        # TODO - k-nearest neighbor version
+        neighbors = filter(lambda n: distance_fn(n.config, new.config) < radius, nodes)
+        nodes.append(new)
+
+        # TODO: smooth solution once found to improve the cost bound
+        for n in neighbors:
+            d = distance_fn(n.config, new.config)
+            if (n.cost + d) < new.cost:
+                path = safe_path(extend_fn(n.config, new.config), collision_fn)
+                if (len(path) != 0) and (distance_fn(new.config, path[-1]) < EPSILON):
+                    new.rewire(n, d, path[:-1], iteration=iteration)
+        for n in neighbors:  # TODO - avoid repeating work
+            d = distance_fn(new.config, n.config)
+            if (new.cost + d) < n.cost:
+                path = safe_path(extend_fn(new.config, n.config), collision_fn)
+                if (len(path) != 0) and (distance_fn(n.config, path[-1]) < EPSILON):
+                    n.rewire(new, d, path[:-1], iteration=iteration)
+
+        if draw_fn:
+            nodes[0].draw(draw_fn, remove_all=True)
+            for n in nodes[1:]:
+                n.draw(draw_fn, remove_all=False)
+
+    if goal_n is None:
+        return None
+    return goal_n.retrace()
+
+def informed_rrt_star_multi_goal(start, goal, distance_fn, sample_fn, extend_fn, collision_fn, radius, **kwargs):
+    return rrt_star_multi_goal(start, goal, distance_fn, sample_fn, extend_fn, collision_fn, radius, informed=True, **kwargs)
--- baselines/run_baseline.py
+++ baselines/run_baseline.py
@@ -3,7 +3,6 @@ import random
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
-from pruning_sb3.pruning_gym.pruning_env import PruningEnv
 from pruning_sb3.pruning_gym.pruning_env import PruningEnvRRT
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.env_util import make_vec_env
@@ -22,20 +21,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     set_args(args, parser)
     parsed_args = vars(parser.parse_args())
-    args_global, args_train, args_test, args_record, args_callback, args_policy, args_env, args_eval, parsed_args_dict = organize_args(
+    args_global, args_train, args_test, args_record, args_callback, args_policy, args_env, args_eval, args_baseline, parsed_args_dict = organize_args(
         parsed_args)
 
     print(parsed_args_dict)
     or_bins = make_or_bins(args_train, "train")
 
-    env = make_vec_env(PruningEnv, env_kwargs=args_record, n_envs=args_global['n_envs'], vec_env_cls=SubprocVecEnv)
     env = make_vec_env(PruningEnvRRT, env_kwargs=args_record, n_envs=args_global['n_envs'], vec_env_cls=SubprocVecEnv)
 
 
 
 
     dataset = None
-    type = "uniform"
+    planner = args_baseline['planner']
+    type = args_baseline['dataset_type']
     if os.path.exists(f"{type}_dataset.pkl"):
         with open(f"{type}_dataset.pkl", "rb") as f:
             dataset = pickle.load(f)
@@ -48,10 +47,10 @@ if __name__ == "__main__":
 
     if set_goal_callback.dataset is None:
         set_goal_callback.make_dataset()
-    random.shuffle(set_goal_callback.dataset)
+    # random.shuffle(set_goal_callback.dataset)
     num_points_per_env = len(set_goal_callback.dataset)//env.num_envs
     for i in range(env.num_envs):
         dataset = set_goal_callback.dataset[i*num_points_per_env:(i+1)*num_points_per_env]
         env.env_method("set_dataset", dataset=dataset, indices=i)
-    results_method = GenerateResults(env, set_goal_callback, save_video = False)
-    results_method.run(set_goal_callback) #keep multiples of n_envs
+    results_method = GenerateResults(env, set_goal_callback, planner, save_video = args_baseline['save_video'])
+    results_method.run(args_baseline['results_save_path']+'.csv') #keep multiples of n_envs
